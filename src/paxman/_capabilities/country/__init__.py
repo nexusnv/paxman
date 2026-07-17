b37a31d641053e48edc1759db67f9cc73fe627ec
Azahari Zaman <azaharizaman@gmail.com>
2026-07-17 20:43:42 +0800

    feat(country): add CountryCapability + validation/discovery wiring

--- src/paxman/_capabilities/country/__init__.py
+++ src/paxman/_capabilities/country/__init__.py
@@ -3,9 +3,13 @@
 
 from __future__ import annotations
 
+from paxman._capabilities.country.canonicalizer import CountryCapability
 from paxman._capabilities.country.contract import CanonicalCountryContract, Country
+from paxman._capabilities.country.rules import _RULE_PROVENANCE
 
 __all__ = [
+    "_RULE_PROVENANCE",
     "CanonicalCountryContract",
     "Country",
+    "CountryCapability",
 ]
